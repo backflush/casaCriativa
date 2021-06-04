f25de24cd873250c75926ccef49fd1a45a96192a
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-04 10:25:56 -0300

    Finished the dashboard controller configuration

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const routes = express.Router();
 
+const DashboardController = require("./controllers/DashboardController");
+
 routes.get("/", DashboardController.index);
 
 module.exports = routes;
