35b136b4688d81b8a2695e1c610327dbd4a494d7
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-07 11:20:59 -0300

    Created the route ideas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const express = require("express");
 const routes = express.Router();
 
 const DashboardController = require("./controllers/DashboardController");
+const IdeasController = require("./controllers/IdeasController");
 
 routes.get("/", DashboardController.index);
+
+routes.get("/ideas", IdeasController.index);
 
 module.exports = routes;
